e9e66165ccc6c09e5d9a55a8c35a06c69d632238
qufsid2 <qufsid2@gmail.com>
2026-09-10 21:07:43 +0900

    Add files via upload

---
+++ ch13-1/file.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main(void)
+{
+    double arr[5];
+    double* ptr;
+    int i;
+
+    ptr = arr;
+
+    for (i = 0; i < 5; i++) {
+        printf("실수 입력: ");
+        scanf("%lf", ptr++);
+    }
+
+    ptr = arr;
+
+    for (i = 0; i < 5; i++) {
+        *ptr += 2;
+        ptr++;
+    }
+
+    ptr = arr;
+
+    printf("\n2씩 증가된 배열의 값:\n");
+
+    for (i = 0; i < 5; i++)
+        printf("%.2f ", *ptr++);
+
+    return 0;
+}
