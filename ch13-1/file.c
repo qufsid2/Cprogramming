f5f6005ac61612c4d157d814200c6f275fa2ab1c
qufsid2 <qufsid2@gmail.com>
2026-09-10 14:32:18 +0900

    Add files via upload

---
+++ ch13-1/file.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main() {
+	int grade[5];
+	int sum = 0, i, average;
+
+	for (i = 0; i < 5; i++) {
+		printf("성적을 입력하시오: ");
+		scanf("%d", &grade[i]);
+		sum += *(grade + i);
+	}
+
+	average = sum / 5.0;
+	
+	printf("성적 평균= %d\n", average);
+	
+	return 0;
+}
