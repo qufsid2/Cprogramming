d369a33c5e2e4a74ae5cbbbf9ca929ab5bd4edd8
qufsid2 <qufsid2@gmail.com>
2026-09-17 01:09:23 +0900

    Add files via upload

---
+++ ch13-2/file.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main(void) {
+	int i = 0;
+	char a[100];
+
+	printf("문자열을 입력하시오: ");
+	scanf("%s", a);
+
+	while (a[i] != '\0') {
+		if (a[i] >= 'A' && a[i] <= 'Z')
+			a[i] += ('a' - 'A');
+		else if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= ('a' - 'A');
+
+		i++;
+	}
+
+	printf("변환 결과: %s \n", a);
+
+	return 0;
+}
