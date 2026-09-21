9e00495e25838f6dfc2697e24c536cb213edb4b3
qufsid2 <qufsid2@gmail.com>
2026-09-22 02:23:09 +0900

    Add files via upload

---
+++ ch14-1/file.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+void swap(int* px, int* py, int* pz);
+
+int main(void) {
+	int x, y, z;
+
+	printf("정수 x를 입력하시오: ");
+	scanf("%d", &x);
+
+	printf("정수 y를 입력하시오: ");
+	scanf("%d", &y);
+
+	printf("정수 z를 입력하시오: ");
+	scanf("%d", &z);
+
+	printf("swap함수 호출 전 x=%d y=%d z=%d \n", x, y, z);
+	swap(&x, &y, &z);
+	printf("swap함수 호출 후 x=%d y=%d z=%d \n", x, y, z);
+
+	return 0;
+}
+
+void swap(int* px, int* py, int* pz) {
+	int tmp;
+
+	tmp = *px;
+	*px = *py;
+	*py = *pz;
+	*pz = tmp;
+}
