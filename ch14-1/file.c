473270c5fd385d9feb04bdf12e18ab78e22e29a2
qufsid2 <qufsid2@gmail.com>
2026-09-22 02:00:55 +0900

    Add files via upload

---
+++ ch14-1/file.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int add2(int value);
+
+int main(void) {
+	int number;
+
+	printf("정수를 입력하세요: ");
+	scanf("%d", &number);
+
+	number = add2(number);
+
+	printf("2만큼 증가한 값: %d \n", number);
+
+	return 0;
+}
+
+int add2(int value) {
+	value += 2;
+
+	return value;
+}
